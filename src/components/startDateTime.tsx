d88e7216b5612da1143a9ff4664b1e831b7c5f92
Kanahiro <mediterranean1769@gmail.com>
2020-10-24 15:38:19 +0900

    rename

--- src/components/startDateTime.tsx
+++
@@ -1,6 +0,0 @@
-const START_TIME = new Date('2020-10-24').getTime();
-const CURRENT_TIME = Date.now();
-const TIME_DIFF = CURRENT_TIME - START_TIME;
-const DAY_FROM_FOUNDATION = Math.floor(TIME_DIFF / (1000 * 60 * 60 * 24));
-
-export { DAY_FROM_FOUNDATION };
