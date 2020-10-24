0b7e821ea8495718b355d9816aef37a11278dd7b
Kanahiro <mediterranean1769@gmail.com>
2020-10-24 15:50:32 +0900

    format

--- src/components/daysFromFoundation.tsx
+++ src/components/daysFromFoundation.tsx
@@ -1,6 +1,6 @@
-const START_TIME = new Date('2020-10-24').getTime();
-const CURRENT_TIME = Date.now();
-const TIME_DIFF = CURRENT_TIME - START_TIME;
-const DAYS_FROM_FOUNDATION = Math.floor(TIME_DIFF / (1000 * 60 * 60 * 24));
+const START_TIME = new Date('2020-10-24').getTime()
+const CURRENT_TIME = Date.now()
+const TIME_DIFF = CURRENT_TIME - START_TIME
+const DAYS_FROM_FOUNDATION = Math.floor(TIME_DIFF / (1000 * 60 * 60 * 24))
 
-export default DAYS_FROM_FOUNDATION;
+export default DAYS_FROM_FOUNDATION
