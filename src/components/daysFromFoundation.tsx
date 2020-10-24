261fce9f1cb68e0d4733ebfeeb9b1d4f958c723d
Kanahiro <mediterranean1769@gmail.com>
2020-10-24 16:23:37 +0900

    format

--- src/components/daysFromFoundation.tsx
+++ src/components/daysFromFoundation.tsx
@@ -1,10 +1,10 @@
-import * as React from 'react';
+import * as React from 'react'
 
-const START_TIME = new Date('2020-10-24').getTime();
-const CURRENT_TIME = Date.now();
-const TIME_DIFF = CURRENT_TIME - START_TIME;
+const START_TIME = new Date('2020-10-24').getTime()
+const CURRENT_TIME = Date.now()
+const TIME_DIFF = CURRENT_TIME - START_TIME
 const DaysFromFoundation = () => (
   <div>{Math.floor(TIME_DIFF / (1000 * 60 * 60 * 24))}</div>
-);
+)
 
-export default DaysFromFoundation;
+export default DaysFromFoundation
