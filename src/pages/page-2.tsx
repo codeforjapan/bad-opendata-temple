392dad1518a824953010bb88cfc914926873f9be
Kazuki Imamura <kaizumaki@gmail.com>
2020-11-01 22:20:02 +0900

    page-2に背景画像を設定

--- src/pages/page-2.tsx
+++ src/pages/page-2.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import Link from 'gatsby-link'
 import Layout from '../components/layout'
+import ContentWrap from '../components/contentWrap'
 import RandomWord from '../components/randomWord'
 import Marquee from '../components/marquee';
 import Button from '../components/button'
@@ -9,7 +10,7 @@ import BellSoundButton from '../components/bellSoundButton'
 
 const SecondPage = () => (
   <Layout>
-    <div>
+    <ContentWrap>
       <h1>Hi from the second page</h1>
       <Button text="marquee" />
       <p>Welcome to page 2</p>
@@ -23,7 +24,7 @@ const SecondPage = () => (
       <Marquee text="BADデータを作ってしまうとその除霊や供養にはその何倍ものコストがかかります。お金は大切に。" />
       <Marquee text="何でもかんでも紙に印刷する時代はもう終わりです。紙前提のフォーマットから抜け出しましょう。" delay="1s" duration="15s" />
       <BellSoundButton />
-    </div>
+    </ContentWrap>
   </Layout>
 )
 
