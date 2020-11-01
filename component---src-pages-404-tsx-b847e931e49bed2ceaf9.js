ee754a19565a5c5bb07eaf835cda0f613ed02ec7
kaizumaki <kaizumaki@users.noreply.github.com>
2020-11-01 13:27:31 +0000

    deploy: f428fa0846292ff34dc50c868c97422d194d101e

---
+++ component---src-pages-404-tsx-b847e931e49bed2ceaf9.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"i6+/":function(e,t,n){"use strict";n.r(t);var u=n("q1tI");t.default=function(){return u.createElement("div",null,u.createElement("h1",null,"NOT FOUND"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-b847e931e49bed2ceaf9.js.map
