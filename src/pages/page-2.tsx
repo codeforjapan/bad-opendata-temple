9d1f1d2e820cec940cc168499d4f3b9fc10d6052
osoken <osoken.devel@outlook.jp>
2020-11-01 15:22:40 +0900

    add button component

--- src/pages/page-2.tsx
+++ src/pages/page-2.tsx
@@ -3,11 +3,13 @@ import Link from 'gatsby-link'
 import Layout from '../components/layout'
 import WordCalendar from '../components/word-calendar'
 import Marquee from '../components/marquee';
+import Button from '../components/button'
 
 const SecondPage = () => (
   <Layout>
     <div>
       <h1>Hi from the second page</h1>
+      <Button text="marquee" />
       <p>Welcome to page 2</p>
       <Link to="/">Go back to the homepage</Link>
       <WordCalendar />
