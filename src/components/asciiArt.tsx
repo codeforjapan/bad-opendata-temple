4e15286c25a1e89f3b2f4f023ba27c1ed77b90d3
maekawa <niche.and.such@gmail.com>
2020-11-12 02:56:05 +0900

    アスキーアートコンポーネントとそれを利用する酒樽コンポーネントを作成 使えるフォントはひとまずStandard,Romam,Larry 3Dとした。 必要であれば`node_modules/figlet/fonts`から`static/fonts`にコピーすれば使えるようになる。

---
+++ src/components/asciiArt.tsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import * as figlet from 'figlet'
+import styled from 'styled-components'
+
+//アスキーアートはpreで囲んでフォントを合わせる
+const AsciiArtPre = styled.pre`
+    line-height: 1rem;
+    font-family: 'PixelMplus10';
+`
+
+interface AsciiArtProps {
+    name: string,
+    font?: figlet.Fonts,
+    decoration?: (param: string) => string
+  }
+
+
+const AsciiArt = ({
+        name= "",
+        font= 'Standard',
+        decoration= (param: string) => param
+    }: AsciiArtProps) => {
+    const [output, setOutput] = useState("");
+
+    useEffect(() => {
+        //アスキーアートを作る
+        figlet.defaults({'fontPath':'/fonts'});
+        figlet.text(name, 
+            { font: font　},
+            function(err, data) {
+                console.log("generate text");
+                console.log(data);
+                if (data) {
+                    setOutput(decoration(data));
+                }
+            }
+        );
+    },[name]);
+
+    return (
+        <AsciiArtPre>
+            {output}
+        </AsciiArtPre>
+    )
+}
+
+export default AsciiArt
--- src/pages/page-2.tsx
+++ src/pages/page-2.tsx
@@ -3,6 +3,8 @@ import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import Marquee from '../components/marquee';
 import AudioPlayer from '../components/audioPlayer'
+import AsciiArt from '../components/asciiArt'
+import Sakadaru from '../components/sakadaru'
 
 const SecondPage = () => (
   <Layout>
@@ -16,6 +18,10 @@ const SecondPage = () => (
     <Marquee text="BADデータを作ってしまうとその除霊や供養にはその何倍ものコストがかかります。お金は大切に。" />
     <Marquee text="何でもかんでも紙に印刷する時代はもう終わりです。紙前提のフォーマットから抜け出しましょう。" delay="1s" duration="15s" />
     <AudioPlayer src="/mp3/pannyashingyou16.mp3" />
+    <Sakadaru name="LGTM" />
+    <Sakadaru name="BOD" />
+    <AsciiArt name="BOD" font="Roman" />
+    <AsciiArt name="Temple" font="Larry 3D" />
   </Layout>
 )
 
